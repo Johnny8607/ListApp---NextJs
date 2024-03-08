5ffdadd452bcaec4b73b277f2da7c297d1026152
Johnny8607 <dangjohnny0543@gmail.com>
2024-03-07 23:05:03 -0500

    Home page

---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NavBar(){
+    return(
+        <nav className="flex justify-between items-center
+         bg-sky-700 px-8 py-3 rounded-lg">
+            <Link className="text-white font-bold" href={"/"}>Johnny's List</Link>
+            <Link className=" bg-sky-200 p-2 rounded-lg" href={"/AddTopic"}>Add Topic</Link>
+        </nav>
+    )
+}
